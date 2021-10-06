6cd84aeee817a1248d295adfce03f5f6fa281e67
devdnia <devdnia@gmail.com>
2021-10-06 13:08:40 +0200

    Config webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
                 test: /\.html$/i,
                 loader: 'html-loader',
                 options: {
+                    minimize: false,
                     sources: false,
                 },
             }
