12602584d6e5756918850991b76da1ee90a035a5
devdnia <devdnia@gmail.com>
2021-10-07 09:54:33 +0200

    webpack OK

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,7 @@
+import '../css/componentes.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
 
-
-export const saludar = ( nombre ) =>{
+export const saludar = ( nombre = 'Sin nombre' ) =>{
 
     console.log( 'Creando etiqueta h1' );
 
@@ -9,4 +10,8 @@ export const saludar = ( nombre ) =>{
 
     document.body.append ( h1 );
 
+    // Img
+    // const img = document.createElement( 'img' );
+    // img.src = webpacklogo;
+    // document.body.append( img );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const HtmlWebPack = require('html-webpack-plugin');
+const HtmlWebPack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
  
  
 module.exports = {
@@ -18,6 +20,20 @@ module.exports = {
                     minimize: false,
                     sources: false,
                 },
+                
+            },
+            {
+                test: /\.css$/i, 
+                exclude: /styles.css$/,
+                use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
             }
         ]
     },
@@ -32,11 +48,16 @@ module.exports = {
             template: './src/index.html',
         }),
         
-        // new HtmlWebPackPlugin({
-        //     title: 'Mi webpack App',
-        //     template: './src/index.html',
-        //     filename: './index.html'
-        // }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {  from: 'src/assets/', to: 'assets/'},
+            ],
+        }),
         
     ]
  
